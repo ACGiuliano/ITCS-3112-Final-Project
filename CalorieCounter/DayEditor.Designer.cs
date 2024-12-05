34bd839c58b8d7b8d658d9f8bfeabf262752fd96
Anthony G <egiulia0@email.cpcc.edu>
2024-12-05 16:49:38 -0500

    Add meal functionality implemented
    Can now add meals and it will populate on the listbox on DayEditor form

--- CalorieCounter/DayEditor.Designer.cs
+++ CalorieCounter/DayEditor.Designer.cs
@@ -31,11 +31,19 @@
             BackBtn = new Button();
             LabelDayInfo = new Label();
             listBoxMeals = new ListBox();
+            RemoveMealBtn = new Button();
+            AddMealBtn = new Button();
+            listBoxFood = new ListBox();
+            comboBoxMealType = new ComboBox();
+            labelCalsConsumed = new Label();
+            LabelOverLimit = new Label();
+            AddFoodBtn = new Button();
+            RemoveFoodBtn = new Button();
             SuspendLayout();
             // 
             // BackBtn
             // 
-            BackBtn.Location = new Point(115, 495);
+            BackBtn.Location = new Point(140, 601);
             BackBtn.Name = "BackBtn";
             BackBtn.Size = new Size(198, 61);
             BackBtn.TabIndex = 4;
@@ -45,7 +53,7 @@
             // LabelDayInfo
             // 
             LabelDayInfo.AutoSize = true;
-            LabelDayInfo.Location = new Point(140, 81);
+            LabelDayInfo.Location = new Point(140, 83);
             LabelDayInfo.Name = "LabelDayInfo";
             LabelDayInfo.Size = new Size(173, 25);
             LabelDayInfo.TabIndex = 5;
@@ -55,16 +63,99 @@
             // 
             listBoxMeals.FormattingEnabled = true;
             listBoxMeals.ItemHeight = 25;
-            listBoxMeals.Location = new Point(140, 152);
+            listBoxMeals.Location = new Point(140, 193);
             listBoxMeals.Name = "listBoxMeals";
             listBoxMeals.Size = new Size(337, 254);
             listBoxMeals.TabIndex = 6;
+            // 
+            // RemoveMealBtn
+            // 
+            RemoveMealBtn.Location = new Point(483, 233);
+            RemoveMealBtn.Name = "RemoveMealBtn";
+            RemoveMealBtn.Size = new Size(112, 34);
+            RemoveMealBtn.TabIndex = 7;
+            RemoveMealBtn.Text = "Remove";
+            RemoveMealBtn.UseVisualStyleBackColor = true;
+            // 
+            // AddMealBtn
+            // 
+            AddMealBtn.Location = new Point(483, 193);
+            AddMealBtn.Name = "AddMealBtn";
+            AddMealBtn.Size = new Size(112, 34);
+            AddMealBtn.TabIndex = 8;
+            AddMealBtn.Text = "Add";
+            AddMealBtn.UseVisualStyleBackColor = true;
+            AddMealBtn.Click += AddMealBtn_Click;
+            // 
+            // listBoxFood
+            // 
+            listBoxFood.FormattingEnabled = true;
+            listBoxFood.ItemHeight = 25;
+            listBoxFood.Location = new Point(645, 193);
+            listBoxFood.Name = "listBoxFood";
+            listBoxFood.Size = new Size(337, 254);
+            listBoxFood.TabIndex = 9;
+            // 
+            // comboBoxMealType
+            // 
+            comboBoxMealType.FormattingEnabled = true;
+            comboBoxMealType.Location = new Point(140, 453);
+            comboBoxMealType.Name = "comboBoxMealType";
+            comboBoxMealType.Size = new Size(223, 33);
+            comboBoxMealType.TabIndex = 10;
+            // 
+            // labelCalsConsumed
+            // 
+            labelCalsConsumed.AutoSize = true;
+            labelCalsConsumed.Location = new Point(140, 154);
+            labelCalsConsumed.Name = "labelCalsConsumed";
+            labelCalsConsumed.Size = new Size(107, 25);
+            labelCalsConsumed.TabIndex = 11;
+            labelCalsConsumed.Text = "Consumed: ";
+            // 
+            // LabelOverLimit
+            // 
+            LabelOverLimit.AutoSize = true;
+            LabelOverLimit.Font = new Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            LabelOverLimit.ForeColor = Color.Red;
+            LabelOverLimit.Location = new Point(411, 9);
+            LabelOverLimit.Name = "LabelOverLimit";
+            LabelOverLimit.Size = new Size(294, 38);
+            LabelOverLimit.TabIndex = 12;
+            LabelOverLimit.Text = "OVER CALORIE LIMIT";
+            LabelOverLimit.Visible = false;
+            // 
+            // AddFoodBtn
+            // 
+            AddFoodBtn.Location = new Point(988, 193);
+            AddFoodBtn.Name = "AddFoodBtn";
+            AddFoodBtn.Size = new Size(112, 34);
+            AddFoodBtn.TabIndex = 13;
+            AddFoodBtn.Text = "Add";
+            AddFoodBtn.UseVisualStyleBackColor = true;
+            // 
+            // RemoveFoodBtn
+            // 
+            RemoveFoodBtn.Location = new Point(988, 233);
+            RemoveFoodBtn.Name = "RemoveFoodBtn";
+            RemoveFoodBtn.Size = new Size(112, 34);
+            RemoveFoodBtn.TabIndex = 14;
+            RemoveFoodBtn.Text = "Remove";
+            RemoveFoodBtn.UseVisualStyleBackColor = true;
             // 
             // DayEditor
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(707, 650);
+            ClientSize = new Size(1158, 742);
+            Controls.Add(RemoveFoodBtn);
+            Controls.Add(AddFoodBtn);
+            Controls.Add(LabelOverLimit);
+            Controls.Add(labelCalsConsumed);
+            Controls.Add(comboBoxMealType);
+            Controls.Add(listBoxFood);
+            Controls.Add(AddMealBtn);
+            Controls.Add(RemoveMealBtn);
             Controls.Add(listBoxMeals);
             Controls.Add(LabelDayInfo);
             Controls.Add(BackBtn);
@@ -79,5 +170,13 @@
         private Button BackBtn;
         private Label LabelDayInfo;
         private ListBox listBoxMeals;
+        private Button RemoveMealBtn;
+        private Button AddMealBtn;
+        private ListBox listBoxFood;
+        private ComboBox comboBoxMealType;
+        private Label labelCalsConsumed;
+        private Label LabelOverLimit;
+        private Button AddFoodBtn;
+        private Button RemoveFoodBtn;
     }
 }
--- CalorieCounter/DayEditor.cs
+++ CalorieCounter/DayEditor.cs
@@ -23,7 +23,61 @@ namespace CalorieCounter
 
         private void DayEditor_Load(object sender, EventArgs e)
         {
+            // Make sure we have selections for meal types
+            comboBoxMealType.Items.AddRange(new string[] { "Breakfast", "Lunch", "Dinner", "Snack" });
+
             LabelDayInfo.Text = $"{selectedDay.dayOfWeek}, {selectedDay.date.ToShortDateString()}\nCalorie Limit: {selectedDay.calorieLimit}";
+
+            // populates the listbox with the meals for the selected day so we can edit or add
+            foreach (Meal meal in selectedDay.Meals)
+            {
+                listBoxMeals.Items.Add($"{meal.mealType} - {meal.totalCalories} calories");
+            }
+
+            UpdateSummary();
+
+            this.FormClosing += DayEditorFormClosing; // gotta make sure I have this in all my forms
+        }
+        /*
+         * This updates the calorie summary at the top of the label box
+         * and if the user is over their calorie limit it will display
+         * a large red text at the top of the screen.
+         */
+        private void UpdateSummary()
+        {
+            labelCalsConsumed.Text = $"Calories Consumed: {selectedDay.consumedCalories} / {selectedDay.calorieLimit}";
+            LabelOverLimit.Visible = selectedDay.overLimit;
+        }
+
+        private void DayEditorFormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void AddMealBtn_Click(object sender, EventArgs e)
+        {
+            string selectedMealType = comboBoxMealType.SelectedItem?.ToString();
+
+            if (!string.IsNullOrEmpty(selectedMealType))
+            {
+                foreach (var item in listBoxMeals.Items)
+                {
+                    if (item.ToString().Contains(selectedMealType))
+                    {
+                        MessageBox.Show("Meal already exists for this day.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        return;
+                    }
+                }
+
+                // first adds to list, then adds to day object
+                listBoxMeals.Items.Add(selectedMealType);
+
+                selectedDay.AddMeal(new Meal(selectedMealType));
+            }
+            else
+            {
+                 MessageBox.Show("Select a meal type from the dropdown.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
